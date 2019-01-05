ed4da1bf260850fc5adb578cdce6f6dde9ec4241
Felice Forby <feliceforby@gmail.com>
2019-01-05 05:10:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching (collection, string) {
   matchingItems = collection.filter( function (item) {
-    item.toLowerCase() === string.toLowerCase();
+    return item.toLowerCase() === string.toLowerCase();
   });
 
   return matchingItems;
