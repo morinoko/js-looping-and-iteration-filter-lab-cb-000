b1cfb8fa8999c6bb5627576f0c9fadb38108ac04
Felice Forby <feliceforby@gmail.com>
2019-01-05 05:07:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Code your solution in this file
-function findMatching () {
-  
+function findMatching (collection, string) {
+  matchingItems = collection.filter( function (item) {
+    item === string;
+  });
+
+  return matchingItems;
 }
 
 function fuzzyMatch () {
-  
+
 }
 
 function matchName () {
