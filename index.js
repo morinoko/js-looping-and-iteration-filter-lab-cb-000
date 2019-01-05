8b8d1c2ac0ed5a70e41b3c50c291bcb7debb2ed1
Felice Forby <feliceforby@gmail.com>
2019-01-05 05:49:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file
+// Return drivers with the given name (case-insensitive)
+function findMatching (driverList, name) {
+  matchingDrivers = driverList.filter( function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+
+  return matchingDrivers;
+}
+
+// Return drivers that have a name that begins with the given string
+function fuzzyMatch (driverList, string) {
+  const pattern = new RegExp("^" + string.toLowerCase());
+
+  matchingDrivers = driverList.filter( function (driver) {
+    return driver.toLowerCase().match(pattern);
+  });
+
+  return matchingDrivers;
+}
+
+// Return driver objects if the given string matches the name key
+function matchName (driverList, searchString) {
+  matchingDrivers = driverList.filter( function (driver) {
+    return driver.name.toLowerCase() === searchString.toLowerCase();
+  });
+
+  return matchingDrivers;
+}
