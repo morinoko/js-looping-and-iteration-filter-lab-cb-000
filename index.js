c19189b8add6682ea52b55fd6b80a8894fe5f706
Felice Forby <feliceforby@gmail.com>
2019-01-05 05:19:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ function findMatching (drivers, name) {
   return matchingItems;
 }
 
-function fuzzyMatch () {
+// Return drivers that have a name that begins with the given string
+function fuzzyMatch (drivers, partialName) {
+  matchingDrivers = drivers.filter( function (driver) {
+    return driver.toLowerCase().includes(partialName, 0);
+  });
 
+  return matchingDrivers;
 }
 
 function matchName () {
