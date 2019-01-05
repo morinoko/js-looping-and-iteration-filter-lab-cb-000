4cf915636c9b14ab0abadc5e418103f6dbc78afb
Felice Forby <feliceforby@gmail.com>
2019-01-05 05:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
-function findMatching (drivers, string) {
+function findMatching (drivers, name) {
   matchingDrivers = drivers.filter( function (driver) {
-    return driver.toLowerCase() === string.toLowerCase();
+    return driver.toLowerCase() === name.toLowerCase();
   });
 
   return matchingItems;
